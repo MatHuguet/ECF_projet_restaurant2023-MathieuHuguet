32f40ebee47aa83d46355fba73eaaf4946192fdb
MathieuH <huguet.mathieu@gmail.com>
2023-03-20 11:30:53 +0100

    define first elements of layout

---
+++ components/navbar.php
@@ -0,0 +1,17 @@
+<?php
+echo
+'<nav class="navbar sticky-top navbar-expand-sm navbar-expand-md navbar-expand-lg">
+    <div class="container">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Réserver</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>';
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Roboto+Condensed:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Restaurant Le Quai Antique -Chambéry</title>
+</head>
+<body>
+    <header>
+        <div class="top-header py-4">
+            <div class="container">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    </header>
+<?php require_once './components/navbar.php'; ?>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
